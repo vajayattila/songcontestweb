905d0de3a9647902137ca779cc62bc17424948a2
ata <ata@ata-K54C>
2017-04-09 11:53:16 +0200

    add config.php

---
+++ php/songcontestweb/application/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *  @file config.php
+ *  @brief Configuration file for SongContestWeb. Project home: https://github.com/vajayattila/songcontestweb
+ *	@author Vajay Attila (vajay.attila@gmail.com)
+ *  @copyright MIT License (MIT)
+ *  @date 2017.04.09
+ *  @version 1.0.0.0
+ */
