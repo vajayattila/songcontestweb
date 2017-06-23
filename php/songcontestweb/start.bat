dd6cfaaffbf8a75d37740a94dba508527df10dbb
Vajay Attila <vajay.attila@gmail.com>
2017-06-23 09:42:00 +0200

    change server port
    set port 8000->8001

--- php/songcontestweb/start.bat
+++ php/songcontestweb/start.bat
@@ -1,5 +1,5 @@
 set PHP_PATH_FOLDER=c:\php-7.1.3-Win32-VC14-x64
-set TEST_HOST=127.0.0.1:8000
+set TEST_HOST=127.0.0.1:8001
 rem set BROWSER_PATH=explorer
 set BROWSER_PATH=D:\Ati\PortableApps\PortableApps\GoogleChromePortable\GoogleChromePortable.exe
 start %PHP_PATH_FOLDER%\php.exe -S %TEST_HOST%
